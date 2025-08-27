bd70b037feaac6e963ac9f1218b6de68874972ff
Nurulshaikh786 <shaikhnurul8200@gmail.com>
2025-08-28 00:05:15 +0530

    PR_2_JS

--- grade.js
+++ grade.js
@@ -1,8 +1,37 @@
-let studentId = 8133 , studentName = "Nurul" , module_1 =  90 , module_2 = 86 , module_3 = 75 , module_4 = 92 ;
 
-let totalMarks = module_1 + module_2 + module_3 + module_4;
+let fullname = "Nurul" ,
+    id = 8133 ,
+    module_1 = 87,
+    module_2 = 96,
+    module_3 = 79,
+    module_4 = 63;
 
 
+ 
+   let totalMarks = module_1 + module_2 + module_3 + module_4 ;
+   console.log(`You got ${totalMarks} from 400`)
 
-console.log(totalMarks);
+   let percentage = totalMarks / 400 * 100;
 
+   console.log(`You got ${percentage}% in exams`);
+   
+
+   let grade;
+
+   if(percentage >= 90){
+      grade = "A+";
+   }
+   else if(percentage >= 80){
+      grade = "A";
+   }
+   else if(percentage >= 70){
+      grade = "B";
+   }
+   else if(percentage >= 60){
+      grade = "C";
+   }
+   else{
+    console.log("You failed this exam!!");
+   }
+
+   console.log(`You got ${grade} in your exam`);
